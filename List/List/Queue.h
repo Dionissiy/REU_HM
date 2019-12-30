47e39538edd7f68c4d0810571caeb0520e729033
Dionissiy <32749678+Dionissiy@users.noreply.github.com>
2019-12-30 07:44:29 +0300

    fix Queue class

--- List/List/Queue.h
+++ List/List/Queue.h
@@ -4,48 +4,14 @@ template <class T, class Container = List<T>>
 class Queue
 {
 public:
-	void push(T data);
-	void pop();
-	const T& front() const;
-	const T& back() const;
-	bool empty() const;
-	int size() const;
+	Queue() : container() { }
+	void push(T data) { container.push_back(data); }
+	void pop() { container.pop_front(); }
+	const T& front() const { return container.front(); }
+	const T& back() const { return container.back(); }
+	bool empty() const { return container.empty(); }
+	int size() const { return container.size(); }
+	void clear() { container.clear(); }
 private:
 	Container container;
 };
-
-template<class T, class Container>
-inline void Queue<T, Container>::push(T data)
-{
-	container.push_back(data);
-}
-
-template<class T, class Container>
-inline void Queue<T, Container>::pop()
-{
-	container.pop_front();
-}
-
-template<class T, class Container>
-inline const T& Queue<T, Container>::front() const
-{
-	return container.front();
-}
-
-template<class T, class Container>
-inline const T& Queue<T, Container>::back() const
-{
-	return container.back();
-}
-
-template<class T, class Container>
-inline bool Queue<T, Container>::empty() const
-{
-	return container.empty();
-}
-
-template<class T, class Container>
-inline int Queue<T, Container>::size() const
-{
-	return container.size();
-}
