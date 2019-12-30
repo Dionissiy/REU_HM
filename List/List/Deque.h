c81585bdaecf137ee023dbc7f0c308e352540d36
Dionissiy <32749678+Dionissiy@users.noreply.github.com>
2019-12-30 07:43:37 +0300

    fix Deque class

--- List/List/Deque.h
+++ List/List/Deque.h
@@ -4,62 +4,16 @@ template <class T, class Container = List<T>>
 class Deque
 {
 public:
-	void push_front(T data);
-	void push_back(T data);
-	void pop_front();
-	void pop_back();
-	const T& front() const;
-	const T& back() const;
-	bool empty() const;
-	int size() const;
+	Deque() : container() { }
+	void push_front(T data) { container.push_front(data); }
+	void push_back(T data) { container.push_back(data); }
+	void pop_front() { container.pop_front(); }
+	void pop_back() { container.pop_back(); }
+	const T& front() const { return container.front(); }
+	const T& back() const { return container.back(); }
+	bool empty() const { return container.empty(); }
+	int size() const { return container.size(); }
+	void clear() { container.clear(); }
 private:
 	Container container;
 };
-
-template<class T, class Container>
-inline void Deque<T, Container>::push_front(T data)
-{
-	container.push_front(data);
-}
-
-template<class T, class Container>
-inline void Deque<T, Container>::push_back(T data)
-{
-	container.push_back(data);
-}
-
-template<class T, class Container>
-inline void Deque<T, Container>::pop_front()
-{
-	container.pop_front();
-}
-
-template<class T, class Container>
-inline void Deque<T, Container>::pop_back()
-{
-	container.pop_back();
-}
-
-template<class T, class Container>
-inline const T& Deque<T, Container>::front() const
-{
-	return container.front();
-}
-
-template<class T, class Container>
-inline const T& Deque<T, Container>::back() const
-{
-	return container.back();
-}
-
-template<class T, class Container>
-inline bool Deque<T, Container>::empty() const
-{
-	return container.empty();
-}
-
-template<class T, class Container>
-inline int Deque<T, Container>::size() const
-{
-	return container.size();
-}
