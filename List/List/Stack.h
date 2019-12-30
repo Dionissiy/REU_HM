5f452403c800acc542f2044d4fd8d3da960ef040
Dionissiy <32749678+Dionissiy@users.noreply.github.com>
2019-12-30 07:45:12 +0300

    fix Stack class

--- List/List/Stack.h
+++ List/List/Stack.h
@@ -5,41 +5,12 @@ class Stack
 {
 public:
 	Stack() : container() {} 
-	void push(const T& data);
-	void pop();
-	const T& top() const;
-	bool empty();
-	int size();
+	void push(const T& data) { container.push_back(data); }
+	void pop() { container.pop_back(); }
+	const T& top() const { return container.back(); }
+	bool empty() { return container.empty(); }
+	int size() { return container.size(); }
+	void clear() { container.clear(); }
 private:
 	Container container;
 };
-
-template<typename T, typename Container>
-inline void Stack<T, Container>::push(const T& data)
-{
-	container.push_back(data);
-}
-
-template<typename T, typename Container>
-inline void Stack<T, Container>::pop()
-{
-	container.pop_back();
-}
-
-template<typename T, typename Container>
-inline const T& Stack<T, Container>::top() const
-{
-	return container.back();
-}
-
-template<typename T, typename Container>
-inline bool Stack<T, Container>::empty()
-{
-	return container.empty();
-}
-
-template<typename T, typename Container>
-inline int Stack<T, Container>::size()
-{
-	return container.size();
-}
